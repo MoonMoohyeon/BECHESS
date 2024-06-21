4ba33b24458a0ae439e80b3fb813582ee99232c5
Kyprosa <fancy@konkuk.ac.kr>
2024-06-21 15:34:14 +0900

    /chess

--- src/main/java/com/example/bechess/controller/GameController.java
+++ src/main/java/com/example/bechess/controller/GameController.java
@@ -1,22 +1,22 @@
-package com.example.bechess.controller;
-
-import com.example.bechess.dto.Move;
-import com.example.bechess.service.GameService;
-import com.example.bechess.service.GameState;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class GameController {
-
-    @Autowired
-    private GameService gameService;
-
-    @MessageMapping("/move")
-    @SendTo("/topic/game")
-    public GameState makeMove(Move move) throws Exception {
-        return gameService.processMove(move);
-    }
-}
+//package com.example.bechess.controller;
+//
+//import com.example.bechess.dto.Move;
+//import com.example.bechess.service.GameService;
+//import com.example.bechess.service.GameState;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class GameController {
+//
+//    @Autowired
+//    private GameService gameService;
+//
+//    @MessageMapping("/move")
+//    @SendTo("/topic/game")
+//    public GameState makeMove(Move move) throws Exception {
+//        return gameService.processMove(move);
+//    }
+//}
