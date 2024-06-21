aae94cc63be855693c8414d3035c8c3b8d1847ed
Kyprosa <fancy@konkuk.ac.kr>
2024-06-21 16:59:42 +0900

    controller

--- src/main/java/com/example/bechess/controller/ChessController.java
+++ src/main/java/com/example/bechess/controller/ChessController.java
@@ -2,6 +2,7 @@ package com.example.bechess.controller;
 
 import com.example.bechess.dto.Move;
 import com.example.bechess.dto.WebSocketMessage;
+import com.example.bechess.service.ChessPiece;
 import com.example.bechess.service.GameState;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -15,9 +16,17 @@ public class ChessController {
     @MessageMapping("/move")
     @SendTo("/topic/gameState")
     public GameState makeMove(WebSocketMessage message) throws Exception {
+        Move move = message.getMove();
+        // Validate move and update gameState
+        // Assuming you have logic to handle the move here
+        ChessPiece piece = gameState.getBoard().get(move.getFrom());
         if (message.getTeam().equals(gameState.getCurrentTeam()) &&
                 message.getRole().equals(gameState.getCurrentRole()) &&
                 gameState.isValidMove(message.getMove())) {
+            gameState.getBoard().put(move.getTo(), piece);
+            gameState.getBoard().remove(move.getFrom());
+            piece.setPosition(move.getTo());
+            gameState.switchPlayer();
             gameState.updateBoard(message.getMove());
             gameState.switchTurn();
         }
